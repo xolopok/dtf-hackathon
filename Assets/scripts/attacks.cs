cbd0ed4498ea3b630a94644b64ee3165640a17f6
Onixilium <16272969+Onixilium@users.noreply.github.com>
2019-11-16 23:10:08 +1000

    добавлен скрипт атаки

---
+++ Assets/scripts/attacks.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attacks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private Vector3  MousePos;
+    // Update is called once per frame
+    void Update()
+    {
+        //здесь получаем координаты мыши которые будут являться входными параметрами для функций атаки
+            
+    }
+
+    //в одну выбранную сторону (куда смотрит мышка)
+    void attack1()
+        {
+            
+        }
+
+    //в две стороны - вверх_И_вниз или влево_И_вправо
+      void attack2()
+        {
+            
+        }
+
+    //ввер в выбранную сторону
+      void attack3()
+        {
+            
+        }
+
+    //расходящийся круг 
+      void attack4()
+        {
+            
+        }
+
+}
