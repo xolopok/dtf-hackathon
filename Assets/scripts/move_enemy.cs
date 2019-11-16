4e30b6add972e218a6d201501786f844c3b50fac
Onixilium <16272969+Onixilium@users.noreply.github.com>
2019-11-16 22:43:23 +1000

    add core 0

---
+++ Assets/scripts/move_enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_enemy : MonoBehaviour
+{
+    float speed = 2f;
+    public GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        gameObject.transform.Translate(0.0f, 0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(gameObject.transform.position,Player.transform.position, Time.deltaTime*speed);
+    }
+
+        void OnTriggerEnter2D(Collider2D col)
+        {
+           // if(other.tag == "Player")
+           Debug.Log($"Столкновение");
+            Destroy(gameObject);
+        }
+
+}
---
+++ Assets/scripts/spawn_manager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_manager : MonoBehaviour
+{
+    public int ran; //количество мобов в волне
+    public  float period_spawn = 3f;//частота спавна в секундах
+    public GameObject enemy; 
+
+    void Start()
+    {        
+       Spawn(0);
+    }
+
+    void Update()
+    {
+        AutoRun();
+    }
+
+    void Spawn(int ran)
+        {
+
+         for (int i=1;i<=ran;i++)
+       {
+            var vec = new Vector3(Random.Range(-20,20),Random.Range(-10,10),0);
+            var qec = new Quaternion(0,0,0,0);
+            GameObject.Instantiate(enemy,vec,qec);               
+       }
+
+        }
+
+   public float elaps = 5f;
+
+    void AutoRun()
+        {
+            elaps-=Time.deltaTime;
+        if(elaps<=0f)
+            {
+            ran = Random.Range(2, 6);  
+             Spawn(ran);
+            elaps=5f;
+            }
+        
+        }
+
+}
