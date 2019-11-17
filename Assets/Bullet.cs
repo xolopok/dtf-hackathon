800ef0cde88b0e4e97f9cc31951d9e676622e016
Alexandr Melnik <xolopok@outlook.com>
2019-11-17 20:12:21 +1000

    Исправлено управление, добавлены модификаторы снарядов, добавлен спавн монстров.

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,31 +4,83 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private Rigidbody2D _rigidbody;
+    public int type = 0;
 
-    void Awake()
-    {
-        this._rigidbody = GetComponent<Rigidbody2D>();
-        Debug.Log("Projectile created at " + this._rigidbody.position);
-    }
+    public int modificator = 0;
 
-    void Update()
+    public GameObject shardPrefab;
+
+    public float explosionForce = 50f;
+
+    private void Update()
     {
-        if (transform.position.magnitude > 10.0f)
+        if (transform.position.magnitude > 50.0f)
         {
             Debug.Log("Projectile travel too long.");
             Destroy(gameObject);
         }
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Projectile collided with " + other.gameObject);
-        Destroy(gameObject);
+
+        var monster = other.gameObject.GetComponent<Monster>();
+        if (monster is null)
+        {
+            return;
+        }
+        else
+        {
+            DamageMonster(monster);
+        }
+
+        switch (this.modificator)
+        {
+            case 0:
+                Destroy(gameObject);
+                break;
+            case 1:
+                break;
+            case 2:
+                Destroy(gameObject);
+                Explode();
+                break;
+        }
     }
 
     public void Launch(Vector2 direction, float force)
     {
-        this._rigidbody.AddForce(direction * force);
+        var rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(direction * force);
+    }
+
+    private void Explode()
+    {
+        var rigidbody = GetComponent<Rigidbody2D>();
+        float angle = 0f;
+        for (int i = 0; i < 6; i++)
+        {
+            SpawnShard(rigidbody.position, Quaternion.Euler(0, 0, angle) * Vector2.up);
+            angle += 60f;
+        }
+    }
+
+    private void SpawnShard(Vector2 castOrigin, Vector2 castDirection)
+    {
+        var bulletObject = Instantiate(this.shardPrefab, castOrigin, Quaternion.identity);
+        var bullet = bulletObject.GetComponent<Bullet>();
+        bullet.type = this.type;
+        bullet.modificator = 0;
+        bullet.Launch(castDirection, this.explosionForce);
+    }
+
+    private void DamageMonster(Monster monster)
+    {
+        monster.health -= (int)(50 * monster.damageFactor);
+        if (monster.health <= 0)
+        {
+            Destroy(monster.gameObject);
+        }
     }
 }
